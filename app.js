1c008639086a79ec4cd69a23ef88bb5b3b289db4
Joanthan Fisher <jonathan.fshr@gmail.com>
2023-11-14 16:15:08 +0100

    completing image section

--- app.js
+++ app.js
@@ -1,9 +1,12 @@
 // WARNING: do not upload your API key onto GITHUB. It can remain in your git commit history as well!
 // this is purely for personal use!
-const API_KEY = 'xyz';
+const API_KEY = '';
 const submitIcon = document.querySelector('#submit-icon');
 const inputElement = document.querySelector('input');
+const imageSection = document.querySelector('.images-section');
 
+//
+//
 const getImages = async () => {
   const options = {
     method: 'POST',
@@ -17,13 +20,24 @@ const getImages = async () => {
       size: '1024x1024',
     }),
   };
+
   try {
     const response = await fetch(
       'https://api.openai.com/v1/images/generations',
       options
     );
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
+
+    // If data exsists -data?
+    data?.data.forEach((imageObject) => {
+      const ImageContainer = document.createElement('div');
+      ImageContainer.classList.add('image-container');
+      const imageElement = document.createElement('img');
+      imageElement.setAttribute('src', imageObject.url);
+      ImageContainer.append(imageElement);
+      imageSection.append(ImageContainer);
+    });
   } catch (error) {
     console.error(error);
   }
